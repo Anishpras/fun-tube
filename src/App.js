4c17d95b3567fd96af993b839dad54089bf355e7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-06-14 23:15:13 +0530

    react-dom done

--- src/App.js
+++ src/App.js
@@ -7,23 +7,35 @@ import HomeScreen from "./screens/homeScreen/HomeScreen";
 import LoginScreen from "./screens/loginScreen/LoginScreen";
 
 import "./_app.scss";
-
-const App = () => {
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggleSidebar = () => toggleSidebar((value) => !value);
 
+  <>
+    <Header handleToggleSidebar={handleToggleSidebar} />
+    <div className="app__container">
+      <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+      <Container fluid className="app__main ">
+        {children}
+      </Container>
+    </div>
+  </>;
+};
+
+const App = () => {
   return (
-    <>
-      <Header handleToggleSidebar={handleToggleSidebar} />
-      <div className="app__container">
-        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
-        <Container fluid className="app__main ">
+    <Router>
+      <Route path="/">
+        <Layout>
           <HomeScreen />
-        </Container>
-      </div>
-    </>
-    // <LoginScreen />
+        </Layout>
+      </Route>
+      <Route path="/auth">
+        <LoginScreen />
+      </Route>
+    </Router>
   );
 };
 
